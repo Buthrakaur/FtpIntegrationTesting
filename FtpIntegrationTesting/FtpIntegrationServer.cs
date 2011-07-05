48bb15af4b7286d25f8db9c7a10d71696970d961
Filip Kinsky <filip@filovo.net>
2011-07-05 16:20:43 +0200

    added option to hide ftpdmin.exe console window

--- FtpIntegrationTesting/FtpIntegrationServer.cs
+++ FtpIntegrationTesting/FtpIntegrationServer.cs
@@ -11,9 +11,15 @@ namespace FtpIntegrationTesting
 	{
 		private readonly Process ftpProcess;
 
-		public FtpIntegrationServer(string rootDirectory, int port = 21)
+		public FtpIntegrationServer(string rootDirectory, int port = 21, bool hideFtpWindow = true)
 		{
-			ftpProcess = Process.Start("ftpdmin.exe", String.Format("-p {0} -ha 127.0.0.1 \"{1}\"", port, rootDirectory));
+			var psInfo = new ProcessStartInfo
+				{
+					FileName = "ftpdmin.exe",
+					Arguments = String.Format("-p {0} -ha 127.0.0.1 \"{1}\"", port, rootDirectory),
+					WindowStyle = hideFtpWindow ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal
+				};
+			ftpProcess = Process.Start(psInfo);
 		}
 
 
